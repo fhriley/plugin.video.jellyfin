6a00a894fca8005eec1090ed8fd321e258d99cd8
Frank Riley <fhriley@gmail.com>
2023-01-11 19:39:14 -0700

    checkpoint

--- source/entrypoint.py
+++ source/entrypoint.py
@@ -1,22 +1,37 @@
 import logging
 import sys
 import traceback
-from typing import Optional
+from typing import Optional, Dict
 
 import requests
 import xbmc
 import xbmcaddon
 
+from lib.addon.entrypoint import get_server
 from lib.addon.log import KodiHandler, LOG_FORMAT
 from lib.addon.settings import Settings
-from lib.generic.api.jellyfin import Server
+from lib.generic.api.jellyfin import Server, User
+from lib.source.items import get_tvshows
 
 _session: Optional[requests.Session] = None
+_user_cache: Optional[Dict[str, User]] = None
+
+
+def authenticate(log: logging.Logger, server: Server, username: str, password: str) -> User:
+    global _user_cache
+    user = _user_cache.get(username)
+    if not user or not server.is_authenticated(user):
+        user = server.authenticate_by_password(username, password)
+        _user_cache[user.user] = user
+    log.debug('authenticate: user=%s', user)
+    return user
 
 
 def main():
     # xbmc.log('============================ start', xbmc.LOGINFO)
     global _session
+    global _user_cache
+
     handle = int(sys.argv[1])
     log = None
 
@@ -39,6 +54,13 @@ def main():
             log.debug('creating new session')
         else:
             log.debug('reusing session')
+
+        if _user_cache is None:
+            _user_cache = {}
+
+        server = get_server(_session, settings, addon)
+        user = authenticate(log, server, settings.get('username'), settings.get('password'))
+        get_tvshows(handle, server, user, debug_level=settings.debug_level)
 
         log.debug('============================ finish')
     except Exception:

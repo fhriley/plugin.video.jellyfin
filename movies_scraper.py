233dca8245982181bc8df05153ca85bb040b0a39
Frank Riley <fhriley@gmail.com>
2023-01-15 13:00:14 -0700

    Fix mains

--- movies_scraper.py
+++ movies_scraper.py
@@ -1,5 +1,7 @@
+from lib.builder.movies import MoviesBuilder
 from lib.router.entrypoint import main
 from lib.router.movies import MoviesRouter
+from lib.scraper.movies import MoviesScraper
 
 if __name__ == '__main__':
-    main(MoviesRouter)
+    main(MoviesRouter, MoviesScraper, MoviesBuilder)
--- tvshows_scraper.py
+++ tvshows_scraper.py
@@ -1,5 +1,7 @@
+from lib.builder.tvshows import TvShowsBuilder
 from lib.router.entrypoint import main
 from lib.router.tvshows import TvShowsRouter
+from lib.scraper.tvshows import TvShowsScraper
 
 if __name__ == '__main__':
-    main(TvShowsRouter)
+    main(TvShowsRouter, TvShowsScraper, TvShowsBuilder)
